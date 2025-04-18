a2683bb545b01d302ef5d51676ba688f663f3e8f
Santiago Hernández <santiagohfi04@gmail.com>
2025-04-17 21:12:01 -0500

    Create platzi market repo for the Spring boot course up to the lesson of creating controllers

---
+++ platzi-market/src/main/java/com/platzi/market/domain/Product.java
@@ -0,0 +1,17 @@
+package com.platzi.market.domain;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product {
+    private int productId;
+    private String name;
+    private int categoryId;
+    private double price;
+    private int stock;
+    private boolean active;
+    private Category category;
+}
---
+++ platzi-market/src/main/java/com/platzi/market/persistence/entity/Compra.java
@@ -0,0 +1,83 @@
+package com.platzi.market.persistence.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+
+public class Compra {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_compra")
+    private Integer idCompra;
+
+    @Column(name = "id_cliente")
+    private String idCliente;
+
+    private LocalDateTime fecha;
+
+    @Column(name = "medio_pago")
+    private Character medioPago;
+
+    private String comentario;
+
+    private Character estado;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente", updatable = false, insertable = false)
+    private Cliente cliente;
+
+    @OneToMany(mappedBy = "compra")
+    private List<ComprasProducto> productos;
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public Character getMedioPago() {
+        return medioPago;
+    }
+
+    public void setMedioPago(Character medioPago) {
+        this.medioPago = medioPago;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public Character getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Character estado) {
+        this.estado = estado;
+    }
+}
