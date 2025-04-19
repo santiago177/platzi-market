e9dd85901072502565440eecabd9cd1ad598f8ab
Santiago Hernández <santiagohfi04@gmail.com>
2025-04-19 17:58:10 -0500

    Implement swagger documentation

--- platzi-market/src/main/java/com/platzi/market/web/controller/PurchaseController.java
+++ platzi-market/src/main/java/com/platzi/market/web/controller/PurchaseController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/purchases")
 public class PurchaseController {
     @Autowired
